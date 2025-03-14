466976d7ac6f06a7fc5e5c4edbe951078e893bf2
dannylit <itsdanlit@gmail.com>
2025-03-14 19:01:01 +0300

    update

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const Token = {
   name: "MLG",
-  decimals(){
-    return "6 decimals"
+  decimals: 6,
+  getDecimals(){
+    return `${this.decimals} decimals`
   }
 }
 const WrapedSol = Object.create(Token)
 
-console.log(WrapedSol.__proto__ === Token)
 
-console.log(WrapedSol.decimals())
+WrapedSol.decimals = 9
+
+class TokenNew{
+  constructor(){
+    this.name = "MLG";
+    this.decimals = 6;
+  }
+  getDecimals(){
+    console.log("decimals",this.decimals)
+  }
+}
+
+class WrapedSolNew extends TokenNew{
+  constructor(){
+    super()
+  }
+  getDecimals(){
+    console.log("decimals",this.decimals)
+  }
+}
+
+
+const wsol = new WrapedSolNew()
+wsol.getDecimals()
+console.log(WrapedSolNew.__proto__)
