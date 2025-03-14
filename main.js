5af11f54e97908a90f70cf8bd567d7a4522b600b
dannylit <itsdanlit@gmail.com>
2025-03-14 16:20:18 +0300

    update

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-class Token{
-  static decimals = 6
-  #decimals
-  constructor(address, decimals){
-    this.address = address,
-    this.#decimals = decimals
+class Message {
+  static totalMessagesSent = 0;
+  static totalMsgLength = 0;
+  
+  constructor(recipient, sender, body) {
+    this.recipient = recipient;
+    this.sender = sender;
+    this.body = body;
+    Message.totalMessagesSent++
+    Message.totalMsgLength += body.length
   }
   
-  static getTokenDecimals(){ 
-    return this.decimals
+  static getAverageMessageLength(){
+    return (Message.totalMessagesSent === 0? 0 : Message.totalMessagesSent/Message.totalMsgLength).toFixed(2)
   }
 }
 
-const memeTokenA = new Token("9TY6DUg1VSssYH5tFE95qoq5hnAGFak4w3cn72sJNCoV", 6)
-const memeTokenB = new Token("E3rijajUad1jSJkvxwSA5YkWvNLTsNhRQTHkcybwKXrC", 7)
-console.log(Token.getTokenDecimals())
+new Message("555-111","555-2222","Fr the Ancients!")
+new Message("555-111","555-2222","Fe Ancients!")
+new Message("555-111","555-2222","F Ancients!")
+new Message("555-111","555-2222","FAncients!")
+new Message("555-111","555-2222","Fncients!")
+new Message("511","555-2222","Fts!")
+console.log(Message.getAverageMessageLength())
